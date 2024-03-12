b99a7822c73a9c6f90e8487b64bc43e866e5f6dc
Vaibhav422 <92776886+Vaibhav992@users.noreply.github.com>
2024-03-12 12:26:21 +0530

    creating a googleAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {GoogleOAuthProvider} from "@react-oauth/google"
 import { Inter, Quicksand } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,7 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={quicksand.className}>
-      <Component {...pageProps} />;
-    </div>
+    <GoogleOAuthProvider clientId="796893770453-cdctenbiseanqgj19b7i99cn2frg0pu6.apps.googleusercontent.com">
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import { BiHash, BiHomeCircle, BiMoney, BiUser } from "react-icons/bi";
 import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 import FeedCard from "@/components/FeedCard";
 import { SlOptions } from "react-icons/sl";
-
-
+import { useCallback } from "react";
 
 interface TwitterSidebarButton {
   title: string;
@@ -48,12 +48,13 @@ const sidebarMenuItems: TwitterSidebarButton[] = [
 ];
 
 export default function Home() {
+  const handleLoginWithGoogle = useCallback((cred: CredentialResponse) => {}, [])
   return (
     <div>
       <div className="grid grid-cols-12 h-screen w-screen px-56">
-        <div className=" col-span-3  pt-1  ml-28">
+        <div className="col-span-3 pt-1 ml-28">
 
-          <div className=" text-2xl h-fit w-fit hover:bg-gray-600 rounded-full p-4 cursor-pointer transition-all ">
+          <div className="text-2xl h-fit w-fit hover:bg-gray-600 rounded-full p-4 cursor-pointer transition-all">
             <BsTwitter />
           </div>
 
@@ -61,7 +62,7 @@ export default function Home() {
             <ul>
               {sidebarMenuItems.map((item) => (
                 <li
-                  className="flex justify-start items-center gap-4  hover:bg-gray-800 rounded-full px-4 py-3 w-fit cursor-pointer mt-2"
+                  className="flex justify-start items-center gap-4 hover:bg-gray-800 rounded-full px-4 py-3 w-fit cursor-pointer mt-2"
                   key={item.title}
                 >
                   <span className="text-3xl">{item.icon}</span>
@@ -69,21 +70,25 @@ export default function Home() {
                 </li>
               ))}
             </ul>
-            <div className="mt-5 px-3 ">
-              <button className="bg-[rgb(29,155,240)] text-lg rounded-full w-full py-2 px-4 font-semibold ">Tweet</button>
+            <div className="mt-5 px-3">
+              <button className="bg-[rgb(29,155,240)] text-lg rounded-full w-full py-2 px-4 font-semibold">Tweet</button>
             </div>
           </div>
         </div>
 
         <div className="col-span-5 border-r-[1px] border-l-[1px] h-screen border-gray-600">
-        <FeedCard/>
-        <FeedCard/>
-        <FeedCard/>
-        <FeedCard/>
-        <FeedCard/>
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
         </div>
         <div className="right-sidebar col-span-3">
-         
+          <div className="border p-5 bg-slate-700 rounded-lg">
+
+            <h1 className="my-2 text-2xl">New To Twitter?</h1>
+            <GoogleLogin onSuccess={(cred) => console.log(cred)} />
+          </div>
         </div>
       </div>
     </div>
